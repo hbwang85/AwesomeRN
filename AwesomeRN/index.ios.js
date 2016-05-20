38068715bce52307b087162745ab5895f4fa52dc
Haibin Wang <haibin.wang@rea-group.com>
2016-05-20 16:57:11 +0800

    Fetch data from server

--- AwesomeRN/index.ios.js
+++ AwesomeRN/index.ios.js
@@ -7,33 +7,24 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  Image,
-  ListView,
   StyleSheet,
+  ListView,
+  Image,
   Text,
   View
 } from 'react-native';
-
-var MOCKED_MOVIES_DATA = [
-  {title: 'Title', year: '2016', poster: {
-    thumbnail: 'http://i.imgur.com/UePbdph.jpg'
-  }},
-];
 
 var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 class AwesomeRN extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      }),
-      loaded: false,
-    };
+      movies: null,
+    }
   }
 
-  ComponentDidMount() {
+  componentDidMount() {
     this.fetchData();
   }
 
@@ -42,54 +33,50 @@ class AwesomeRN extends Component {
     .then((response) => response.json())
     .then((responseData) => {
       this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
-        loaded: true,
+        movies: responseData.movies,
       });
     })
     .done();
   }
 
+
   render() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
+    if (!this.state.movies) {
+      return this.renderLoadingView()
     }
 
-    // var movie = MOCKED_MOVIES_DATA[0];
-    // return this.renderMovie(movie);
+    //TODO How to invoke the action below
+    var movie = this.state.movies[0]
+    return this.renderMovie(movie);
+}
+
+  renderLoadingView() {
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderMovie}
-        style={styles.listView}
-        />
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
     );
   }
 
-renderLoadingView() {
-  return (
-    <View style={styles.container}>
-      <Text>
-        Loading movies...
-      </Text>
-    </View>
-  );
+  renderMovie(movie) {
+    console.log(movie);
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
-renderMovie(movie) {
-  return (
-    <View style={styles.container}>
-    <Image
-      source={{uri: 'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg'}}
-      style={styles.thumbnail}
-    />
-    <View style={styles.rightContainer}>
-      <Text style={styles.title}>{movie.title}</Text>
-      <Text style={styles.year}>{movie.year}</Text>
-    </View>
-  </View>
-  );
-}
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -99,18 +86,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  rightContainer: {
-    flex: 1,
-  },
-
-  listView: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF'
-  }
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
   title: {
     fontSize: 20,
     marginBottom: 8,
@@ -118,6 +93,23 @@ const styles = StyleSheet.create({
   },
   year: {
     textAlign: 'center',
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
 
